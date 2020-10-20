12c6be4c528dd8463a8a930f28fa5ad620ce8820
Ivan Bergonzani <ivan.bergonzani@outlook.com>
2020-10-20 17:13:52 +0000

    buffer + torque signal filtering

--- include/inria_wbc/utils/torque_safety.hpp
+++ include/inria_wbc/utils/torque_safety.hpp
@@ -8,37 +8,93 @@
 class TorqueCollisionDetection
 {
 public:
-    TorqueCollisionDetection(std::vector<std::string> joints, double threshold=1.0, int buffer_len=1);
 
+    typedef Eigen::Array<bool,-1,-1> ArrayXb;
+
+    typedef struct { 
+        Eigen::VectorXd operator()(const Eigen::MatrixXd& x) { return x.rowwise().mean(); } 
+    } MeanFilter; 
+
+    typedef struct { 
+        Eigen::VectorXd operator()(const Eigen::MatrixXd& x) 
+        { 
+            int c = x.cols(), r = x.rows();
+
+            Eigen::VectorXd res(r);
+            for(int i=0; i<r; ++i)
+            {
+                Eigen::VectorXd v = x.row(i).eval();
+                double* v_ptr = v.data();
+                std::sort(v_ptr, v_ptr+c);
+                
+                if(c % 2 == 0)
+                    res(i) = (v(c/2) + v(c/2-1)) / 2;
+                else
+                    res(i) = v(c/2);
+            }
+            return res;
+        } 
+    } MedianFilter; 
+
+
+    TorqueCollisionDetection(std::vector<std::string> joints, double threshold=1.0, int buffer_len=1);
+    TorqueCollisionDetection(std::vector<std::string> joints, Eigen::VectorXd threshold, int buffer_len=1);
     ~TorqueCollisionDetection() = default;
 
-    bool safety_check(const Eigen::VectorXd& target, const Eigen::VectorXd& sensors);
+    template <typename FilterFunctor = MeanFilter>
+    bool check(const Eigen::VectorXd& target, const Eigen::VectorXd& sensors, FilterFunctor filter);
+    bool check(const Eigen::VectorXd& target, const Eigen::VectorXd& sensors);
 
     void set_threshold(double threshold);
+    void set_threshold(const Eigen::VectorXd& threshold);
 
-    Eigen::VectorXd get_discrepancy();
+    Eigen::VectorXd get_threshold() const;
+
+    Eigen::VectorXd get_discrepancy() const;
+
+    Eigen::VectorXd get_filtered_sensors() const;
+
+    std::vector<int> get_invalid_ids() const;
+
 
 private:
-    double _threshold;
+
+    int _nvar;
     int _buffer_len;
-    int _counter;
+    int _buffer_count;
 
+    Eigen::MatrixXd _buffer;
+    Eigen::VectorXd _threshold;
     Eigen::VectorXd _discrepancy;
-
-    std::vector<Eigen::VectorXd> _buffer;
+    Eigen::VectorXd _filtered_sensors;
+    ArrayXb _validity;
 
 };
 
 
-inline void TorqueCollisionDetection::set_threshold(double threshold)
-{
-    _threshold = threshold;
-}
 
-
-inline Eigen::VectorXd TorqueCollisionDetection::get_discrepancy()
+template <typename FilterFunctor>
+bool TorqueCollisionDetection::check(const Eigen::VectorXd& target, const Eigen::VectorXd& sensors, FilterFunctor filter)
 {
-    return _discrepancy;
+    // keep an ordered buffer for denoising and get denoised sensor value
+    if(_buffer_count < _buffer_len)
+    {
+        _buffer.col(_buffer_count++) = sensors;
+        _filtered_sensors = filter(_buffer.leftCols(_buffer_count));
+    }
+    else
+    {
+        _buffer.leftCols(_buffer_len-1) = _buffer.rightCols(_buffer_len-1).eval();
+        _buffer.rightCols<1>() = sensors;
+        _filtered_sensors = filter(_buffer);
+    }
+
+    // compare with the target
+    _discrepancy = (target - _filtered_sensors).cwiseAbs();
+
+    _validity = _discrepancy.array() < _threshold.array();
+
+    return _validity.all();
 }
 
 
--- src/utils/torque_safety.cpp
+++ src/utils/torque_safety.cpp
@@ -3,28 +3,68 @@
 #include <iostream>
 
 
-
 TorqueCollisionDetection::TorqueCollisionDetection(std::vector<std::string> joints, double threshold, int buffer_len)
-:   _threshold(threshold),
+:   _nvar(joints.size()),
     _buffer_len(buffer_len),
-    _counter(0),
-    _buffer(_buffer_len)
+    _buffer_count(0),
+    _buffer(_nvar, _buffer_len),
+    _threshold(Eigen::VectorXd::Constant(_nvar, threshold)),
+    _discrepancy(_nvar),
+    _filtered_sensors(_nvar)
 { }
 
 
-bool TorqueCollisionDetection::safety_check(const Eigen::VectorXd& target, const Eigen::VectorXd& sensors)
-{
-    _buffer[_counter++ % _buffer_len] = sensors;
+TorqueCollisionDetection::TorqueCollisionDetection(std::vector<std::string> joints, Eigen::VectorXd threshold, int buffer_len)
+:   _nvar(joints.size()),
+    _buffer_len(buffer_len),
+    _buffer_count(0),
+    _buffer(_nvar, _buffer_len),
+    _threshold(threshold),
+    _discrepancy(_nvar),
+    _filtered_sensors(_nvar)
+{ }
 
+
+bool TorqueCollisionDetection::check(const Eigen::VectorXd& target, const Eigen::VectorXd& sensors)
+{
     _discrepancy = (target - sensors).cwiseAbs();
 
-    return _discrepancy.maxCoeff() < _threshold;
+    _validity = _discrepancy.array() < _threshold.array();
+
+    return _validity.all();
 }
 
 
-//
-// RobotTorqueCollisionDetection::RobotTorqueCollisionDetection()
-// :
-// {}
-//
-// RobotTorqueCollisionDetection::safety
+void TorqueCollisionDetection::set_threshold(double threshold)
+{
+    _threshold = Eigen::VectorXd::Constant(_nvar, threshold);
+}
+
+
+void TorqueCollisionDetection::set_threshold(const Eigen::VectorXd& threshold)
+{
+    assert("Wrong size for the threshold vector." && _nvar == threshold.size());
+    _threshold = threshold;
+}
+
+
+Eigen::VectorXd TorqueCollisionDetection::get_discrepancy() const
+{
+    return _discrepancy;
+}
+
+
+Eigen::VectorXd TorqueCollisionDetection::get_filtered_sensors() const
+{
+    return _filtered_sensors;
+}
+
+
+std::vector<int> TorqueCollisionDetection::get_invalid_ids() const
+{
+    std::vector<int> ids;
+    for(int i=0; i<_validity.size(); ++i)
+        if(!_validity(i))
+            ids.push_back(i);
+    return ids;
+}
