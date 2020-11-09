d666ff4ca3a6e84e33d16fb88440dd414d44fd53
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-11-09 16:01:45 +0100

    remove utils::to_euler (not used anymore)

--- include/inria_wbc/utils/utils.hpp
+++ include/inria_wbc/utils/utils.hpp
@@ -15,38 +15,6 @@
 // source https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 namespace inria_wbc {
     namespace utils {
-        struct Quaternion {
-            double w, x, y, z;
-        };
-
-        struct EulerAngles {
-            double roll, pitch, yaw;
-        };
-
-        inline EulerAngles to_euler(Quaternion q)
-        {
-            EulerAngles angles;
-
-            // roll (x-axis rotation)
-            double sinr_cosp = 2 * (q.w * q.x + q.y * q.z);
-            double cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y);
-            angles.roll = std::atan2(sinr_cosp, cosr_cosp);
-
-            // pitch (y-axis rotation)
-            double sinp = 2 * (q.w * q.y - q.z * q.x);
-            if (std::abs(sinp) >= 1)
-                angles.pitch = std::copysign(M_PI / 2, sinp); // use 90 degrees if out of range
-            else
-                angles.pitch = std::asin(sinp);
-
-            // yaw (z-axis rotation)
-            double siny_cosp = 2 * (q.w * q.z + q.x * q.y);
-            double cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
-            angles.yaw = std::atan2(siny_cosp, cosy_cosp);
-
-            return angles;
-        }
-
         template <typename type>
         inline bool parse(type& parameter, std::string parameterName, YAML::Node& config, std::string prevName, bool verbose = true)
         {
