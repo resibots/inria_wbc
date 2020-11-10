5e3d03f37c9f097a3afac864898dccf4bd2f7bef
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-11-10 10:50:24 +0100

    error in PROJECT_NAME_UC

--- example_project/create_project.sh
+++ example_project/create_project.sh
@@ -12,7 +12,7 @@ FILES=$(find . -type f|grep -v $0)
 for i in $FILES; do
     echo "Replacing in $i..."
     sed -i -e "s/@project_name@/$PROJECT_NAME/g" $i
-    sed -i -e "s/@PROJECT_NAME@/$PROJECT_NAME_UC/g"
+    sed -i -e "s/@PROJECT_NAME@/$PROJECT_NAME_UC/g" $i
 done
 mv cmake/example_project.cmake.in cmake/$PROJECT_NAME.cmake.in 
 mv cmake/example_projectConfigVersion.cmake.in cmake/${PROJECT_NAME}ConfigVersion.cmake.in 
