0e4f0b043c9f94ec276cdfa9f75e49b55424d7fa
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-11-10 10:40:29 +0100

    before first test

--- example_project/create_project.sh
+++ example_project/create_project.sh
@@ -9,5 +9,8 @@ for i in $FILES; do
     sed -i -e s/@project_name@/$PROJECT_NAME/g
     sed -i -e s/@PROJECT_NAME@/$PROJECT_NAME_UC/g
 done
+mv cmake/example_project.cmake.in cmake/$PROJECT_NAME.cmake.in 
+mv cmake/example_projectConfigVersion.cmake.in cmake/{$PROJECT_NAME}ConfigVersion.cmake.in 
 
+    
     
