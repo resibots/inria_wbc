2a0fbcff89811a1ecea8fe1e4b8d5acc406071d5
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-11-10 10:49:23 +0100

    correct filetype

--- example_project/create_project.sh
+++ example_project/create_project.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -x
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 name_of_project" >&2
   exit 1
@@ -8,14 +8,14 @@ fi
 PROJECT_NAME=$1
 PROJECT_NAME_UC=echo $1 | tr '[:lower:]' '[:upper:]'
 
-FILES=$(find -fstype f .)
+FILES=$(find . -type f|grep -v $0)
 for i in $FILES; do
     echo "Replacing in $i..."
-    sed -i -e s/@project_name@/$PROJECT_NAME/g
-    sed -i -e s/@PROJECT_NAME@/$PROJECT_NAME_UC/g
+    sed -i -e "s/@project_name@/$PROJECT_NAME/g" $i
+    sed -i -e "s/@PROJECT_NAME@/$PROJECT_NAME_UC/g"
 done
 mv cmake/example_project.cmake.in cmake/$PROJECT_NAME.cmake.in 
-mv cmake/example_projectConfigVersion.cmake.in cmake/{$PROJECT_NAME}ConfigVersion.cmake.in 
+mv cmake/example_projectConfigVersion.cmake.in cmake/${PROJECT_NAME}ConfigVersion.cmake.in 
 
     
     
