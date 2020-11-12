ee9a70435b67d2d369b96d27e0e12be4e220f078
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2020-11-12 17:28:01 +0100

    IWBC_ASSERT with a single arg (like standard assert)

--- include/inria_wbc/exceptions.hpp
+++ include/inria_wbc/exceptions.hpp
@@ -6,12 +6,14 @@
 
 // usage: throw IWBC_EXCEPTION("error:", 42)
 #define IWBC_EXCEPTION(...) inria_wbc::Exception(__FILE__, __LINE__, __VA_ARGS__)
+#define IWBC_FIRST_ARG(N, ...) N
+#define IWBC_FIRST_ARG_STR(N, ...) #N
 
 // usage: assert(x < 3, "we received x=", x)
-#define IWBC_ASSERT(expr, ...)                        \
-    {                                                 \
-        if (!(expr))                                  \
-            throw IWBC_EXCEPTION(#expr, __VA_ARGS__); \
+#define IWBC_ASSERT(...)                                                             \
+    {                                                                                \
+        if (!(IWBC_FIRST_ARG(__VA_ARGS__)))                                          \
+            throw IWBC_EXCEPTION(IWBC_FIRST_ARG_STR(__VA_ARGS__), " ", __VA_ARGS__); \
     }
 
 namespace inria_wbc {
@@ -42,6 +44,10 @@ namespace inria_wbc {
         std::string _make_msg(const char* m, Targs... Fargs) const
         {
             return std::string(m) + _make_msg(Fargs...);
+        }
+        std::string _make_msg() const
+        {
+            return std::string("");
         }
     };
 } // namespace inria_wbc
