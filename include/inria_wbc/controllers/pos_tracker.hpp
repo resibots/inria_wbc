77284d7e5bc6785dcdd0704b3ce452cff6c1e6b3
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2021-02-24 13:11:48 +0100

    for some reason, tsid::getReference() does not return a const ref...

--- include/inria_wbc/controllers/pos_tracker.hpp
+++ include/inria_wbc/controllers/pos_tracker.hpp
@@ -38,7 +38,7 @@ namespace inria_wbc {
 
             pinocchio::SE3 get_se3_ref(const std::string& task_name);
             // we do not return the velocity for now
-            const tsid::math::Vector3& get_com_ref() { return com_task()->getReference().pos; }
+            const tsid::math::Vector3 get_com_ref() { return com_task()->getReference().pos; }
             void set_com_ref(const tsid::math::Vector3& ref) { com_task()->setReference(to_sample(ref)); }
             void set_com_ref(const tsid::trajectories::TrajectorySample& ref) { com_task()->setReference(ref); }
             void set_momentum_ref(const tsid::trajectories::TrajectorySample& ref) { momentum_task()->setReference(ref); }
