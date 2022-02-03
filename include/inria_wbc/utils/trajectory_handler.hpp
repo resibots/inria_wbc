50584443483a74253082855c74aa46cdc25df7f2
BERGONZANI Ivan <ivan.bergonzani@inria.fr>
2022-02-03 17:35:18 +0100

    [wip] derivative for trajectory generation

--- include/inria_wbc/utils/trajectory_handler.hpp
+++ include/inria_wbc/utils/trajectory_handler.hpp
@@ -12,20 +12,67 @@
 
 namespace trajectory_handler {
 
-    inline Eigen::VectorXd minimum_jerk_polynom(const Eigen::VectorXd& x0, const Eigen::VectorXd& xf, double t, double trajectory_duration)
+    namespace order
+    {
+        constexpr uint16_t ZERO = 0;
+        constexpr uint16_t FIRST = 1;
+        constexpr uint16_t SECOND = 2;
+    }
+
+    template <uint16_t d_order>
+    Eigen::VectorXd minimum_jerk_polynom(const Eigen::VectorXd& x0, const Eigen::VectorXd& xf, double t, double trajectory_duration)
+    {
+       IWBC_ERROR("minimum_jerk_polynom is not implemented for " + std::to_string(d_order) + " derivative order.");
+    }
+
+    template <>
+    inline Eigen::VectorXd minimum_jerk_polynom<order::ZERO>(const Eigen::VectorXd& x0, const Eigen::VectorXd& xf, double t, double trajectory_duration)
     {
         assertm(x0.size() == xf.size(), "minimum_jerk_polynom x0 and xf should have the same size");
         double td = t / trajectory_duration;
-        Eigen::VectorXd xt = x0 + (xf - x0) * (6 * td * td * td * td * td - 15 * td * td * td * td + 10 * td * td * td);
+        double td_pow_3 = std::pow(td, 3);
+
+        Eigen::VectorXd xt = x0 + (xf - x0) * (6 * td * td * td_pow_3 - 15 * td * td_pow_3 + 10 * td_pow_3);
         return xt;
     }
 
+    // first order derivative value of minimum jerk polynom at time t
+    template <>
+    inline Eigen::VectorXd minimum_jerk_polynom<order::FIRST>(const Eigen::VectorXd& x0, const Eigen::VectorXd& xf, double t, double trajectory_duration)
+    {
+        assertm(x0.size() == xf.size(), "minimum_jerk_polynom x0 and xf should have the same size");
+        double td = t / trajectory_duration;
+        double td_pow_2 = td * td;
+        Eigen::VectorXd xt = (xf - x0) * (30 * td * td * td_pow_2 - 60 * td * td_pow_2  + 30 * td_pow_2);
+        return xt;
+    }
+
+    // second order derivative value of minimum jerk polynom at time t
+    template <>
+    inline Eigen::VectorXd minimum_jerk_polynom<order::SECOND>(const Eigen::VectorXd& x0, const Eigen::VectorXd& xf, double t, double trajectory_duration)
+    {
+        assertm(x0.size() == xf.size(), "minimum_jerk_polynom x0 and xf should have the same size");
+        double td = t / trajectory_duration;
+        Eigen::VectorXd xt = (xf - x0) * (120 * td * td * td - 120 * td * td  + 60 * td);
+        return xt;
+    }
+
+    inline Eigen::VectorXd minimum_jerk_polynom(const Eigen::VectorXd& x0, const Eigen::VectorXd& xf, double t, double trajectory_duration)
+    {
+        return minimum_jerk_polynom<order::ZERO>(x0, xf, t, trajectory_duration);
+    }
+
+
+    template <uint8_t DO = order::ZERO>
     inline std::vector<Eigen::VectorXd> compute_traj(const Eigen::VectorXd& start, const Eigen::VectorXd& dest, double dt, double trajectory_duration)
     {
+        if(DO > order::SECOND)
+            IWBC_ERROR("compute_traj is not implemented for " + std::to_string(DO) + " derivative order.");
+        
         uint n_steps = std::floor(trajectory_duration / dt);
         std::vector<Eigen::VectorXd> trajectory(n_steps);
         for (uint i = 0; i < n_steps; i++)
-            trajectory[i] = minimum_jerk_polynom(start, dest, dt * i, trajectory_duration);
+            trajectory[i] = minimum_jerk_polynom<DO>(start, dest, dt * i, trajectory_duration);
         return trajectory;
     }
 
