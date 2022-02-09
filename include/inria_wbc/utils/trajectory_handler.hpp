4b3e3e7419343498efbf37f5237750a7e239b5fc
Ivan Bergonzani <ivan.bergonzani@outlook.com>
2022-02-09 18:09:49 +0100

    SE3 Trajectory with derivative (using angle axis interpolation)

--- include/inria_wbc/utils/trajectory_handler.hpp
+++ include/inria_wbc/utils/trajectory_handler.hpp
@@ -63,16 +63,82 @@ namespace trajectory_handler {
     }
 
 
-    template <uint8_t DO = order::ZERO>
+    template <uint16_t ORDER = order::ZERO>
     inline std::vector<Eigen::VectorXd> compute_traj(const Eigen::VectorXd& start, const Eigen::VectorXd& dest, double dt, double trajectory_duration)
     {
-        if(DO > order::SECOND)
-            IWBC_ERROR("compute_traj is not implemented for " + std::to_string(DO) + " derivative order.");
+        if(ORDER > order::SECOND)
+            IWBC_ERROR("compute_traj is not implemented for derivative of order " + std::to_string(ORDER) + ".");
         
         uint n_steps = std::floor(trajectory_duration / dt);
         std::vector<Eigen::VectorXd> trajectory(n_steps);
         for (uint i = 0; i < n_steps; i++)
-            trajectory[i] = minimum_jerk_polynom<DO>(start, dest, dt * i, trajectory_duration);
+            trajectory[i] = minimum_jerk_polynom<ORDER>(start, dest, dt * i, trajectory_duration);
+        return trajectory;
+    }
+
+    // inline std::vector<pinocchio::SE3> compute_traj(const pinocchio::SE3& start, const pinocchio::SE3& dest, double dt, double trajectory_duration)
+    // {
+    //     Eigen::Vector3d pos_start, pos_dest, pos_xt;
+    //     pos_start = start.translation();
+    //     pos_dest = dest.translation();
+    //     Eigen::Quaterniond quat_start(start.rotation());
+    //     Eigen::Quaterniond quat_dest(dest.rotation());
+
+    //     // Interpolate time as min jerk to use in slerp routine (otherwise slerp gives constant angular velocity result)
+    //     Eigen::VectorXd t_start(1), t_dest(1), t_xt(1);
+    //     t_start << 0.;
+    //     t_dest << 1.;
+
+    //     uint n_steps = std::floor(trajectory_duration / dt);
+    //     std::vector<pinocchio::SE3> trajectory;
+
+    //     for (uint i = 0; i < n_steps; i++) {
+    //         // Min jerk trajectory for translation
+    //         pos_xt = minimum_jerk_polynom(pos_start, pos_dest, dt * i, trajectory_duration);
+    //         t_xt = minimum_jerk_polynom(t_start, t_dest, dt * i, trajectory_duration);
+    //         // Slerp interpolation for quaternion
+    //         Eigen::Quaterniond quat_xt = quat_start.slerp(t_xt(0), quat_dest);
+    //         pinocchio::SE3 xt = pinocchio::SE3(quat_xt, pos_xt);
+    //         trajectory.push_back(xt);
+    //     }
+    //     return trajectory;
+    // }
+
+
+    template<uint16_t ORDER>
+    inline std::vector<Eigen::VectorXd> compute_traj(const pinocchio::SE3& start, const pinocchio::SE3& dest, double dt, double trajectory_duration)
+    {
+        if(ORDER > order::SECOND || ORDER < order::FIRST)
+            IWBC_ERROR("compute_traj is not implemented for derivative of order " + std::to_string(ORDER) + ".");
+
+        Eigen::Vector3d pos_start, pos_dest, pos_d_xt;
+        pos_start = start.translation();
+        pos_dest = dest.translation();
+
+        Eigen::AngleAxisd aa_rot(start.rotation().transpose() * dest.rotation());
+        const Eigen::Vector3d& rot_axis = aa_rot.axis();
+        double rot_angle = aa_rot.angle();
+
+        // Interpolate time as min jerk to use in slerp routine (otherwise slerp gives constant angular velocity result)
+        Eigen::VectorXd ang_start(1), ang_dest(1), ang_d_xt(1);
+        ang_start << 0.;
+        ang_dest << rot_angle;
+
+        uint n_steps = std::floor(trajectory_duration / dt);
+        std::vector<Eigen::VectorXd> trajectory;
+
+        for (uint i = 0; i < n_steps; i++) {
+            Eigen::Matrix<double,6,1> d_vec;
+
+            // Min jerk trajectory for translation
+            pos_d_xt = minimum_jerk_polynom<ORDER>(pos_start, pos_dest, dt * i, trajectory_duration);
+            ang_d_xt = minimum_jerk_polynom<ORDER>(ang_start, ang_dest, dt * i, trajectory_duration);
+
+            d_vec.head<3>() = pos_d_xt;
+            d_vec.tail<3>() = (ang_d_xt(0) * rot_axis);
+
+            trajectory.push_back(d_vec);
+        }
         return trajectory;
     }
 
@@ -81,24 +147,28 @@ namespace trajectory_handler {
         Eigen::Vector3d pos_start, pos_dest, pos_xt;
         pos_start = start.translation();
         pos_dest = dest.translation();
-        Eigen::Quaterniond quat_start(start.rotation());
-        Eigen::Quaterniond quat_dest(dest.rotation());
+
+        Eigen::AngleAxisd aa_rot(start.rotation().transpose() * dest.rotation());
+        const Eigen::Vector3d& rot_axis = aa_rot.axis();
+        double rot_angle = aa_rot.angle();
 
         // Interpolate time as min jerk to use in slerp routine (otherwise slerp gives constant angular velocity result)
-        Eigen::VectorXd t_start(1), t_dest(1), t_xt(1);
-        t_start << 0.;
-        t_dest << 1.;
+        Eigen::VectorXd ang_start(1), ang_dest(1), ang_xt(1);
+        ang_start << 0.;
+        ang_dest << rot_angle;
 
         uint n_steps = std::floor(trajectory_duration / dt);
         std::vector<pinocchio::SE3> trajectory;
 
         for (uint i = 0; i < n_steps; i++) {
-            // Min jerk trajectory for translation
+
+            // Min jerk trajectory for translation and rotation
             pos_xt = minimum_jerk_polynom(pos_start, pos_dest, dt * i, trajectory_duration);
-            t_xt = minimum_jerk_polynom(t_start, t_dest, dt * i, trajectory_duration);
-            // Slerp interpolation for quaternion
-            Eigen::Quaterniond quat_xt = quat_start.slerp(t_xt(0), quat_dest);
-            pinocchio::SE3 xt = pinocchio::SE3(quat_xt, pos_xt);
+            ang_xt = minimum_jerk_polynom(ang_start, ang_dest, dt * i, trajectory_duration);
+
+            Eigen::Matrix3d rot_xt = start.rotation() * Eigen::AngleAxisd(ang_xt(0), rot_axis).toRotationMatrix();
+
+            pinocchio::SE3 xt = pinocchio::SE3(rot_xt, pos_xt);
             trajectory.push_back(xt);
         }
         return trajectory;
@@ -112,5 +182,6 @@ namespace trajectory_handler {
         std::fill(trajectory.begin(), trajectory.end(), p);
         return trajectory;
     }
+
 } // namespace trajectory_handler
 #endif
